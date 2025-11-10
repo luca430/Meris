f690e9ceb8a60aa64d317d4aeb8fb9f34e9ee3a9
Luca <lucallegri.98@gmail.com>
2025-11-10 16:28:45 +0100

    modified taylor

--- julia-code/src/dataframes/taylor.jl
+++ julia-code/src/dataframes/taylor.jl
@@ -14,22 +14,22 @@ using StatsBase
 function compute(df::DataFrame, idcolname; minoccupancy::Float64=1e-2, maxfrequency::Float64=1e-2)
     #~ Compute the (log) relative frequency of each of the "species"
     @transform!(df, :frequency = :counts ./ :nreads)
-    @transform!(df, :logfrequency = log.(:frequency))
+    # @transform!(df, :logfrequency = log.(:frequency))
     #~ Compute some summary statistics
-    nsamples = length(unique(df[!,:sample_id]))
+    nsamples = length(unique(df.sample_id))
     minoccupancy = minoccupancy < 1 ./ nsamples ? 1 ./ nsamples : minoccupancy
     sdf = @chain df begin
         @by(
             idcolname,
             :noccurences = length($idcolname),
-            :occupancy = length($(idcolname)) ./ nsamples,
             :meanfrequency = mean(:frequency),
             :varfrequency = var(:frequency, corrected=false),
-            :thirdmomentfrequency = mean((:frequency .- mean(:frequency)).^3),
-            :fourthmomentfrequency = mean((:frequency .- mean(:frequency)).^4)
+            :thirdmomentfrequency = mean(:frequency .^ 3),
+            :fourthmomentfrequency = mean(:frequency  .^ 4)
         )
-        @transform(:rho = sqrt.(:varfrequency .* (:fourthmomentfrequency .- :varfrequency.^2)))
-        @transform(:rho = :thirdmomentfrequency ./ :rho)                                       
+        @transform(:occupancy = :noccurences ./ nsamples)
+        @transform(:rho = sqrt.(:varfrequency .* (:fourthmomentfrequency .- :varfrequency .^ 2)))
+        @transform(:rho = :thirdmomentfrequency ./ :rho)
         #~ Omit those with an occupancy below a specified threshold
         #~ note: if the threshold is zero, then omit hapax legomenas
         #        (those that occur only a single time within the entire document)
@@ -41,13 +41,14 @@ function compute(df::DataFrame, idcolname; minoccupancy::Float64=1e-2, maxfreque
         #~ Take the occupation number into account
         #~ this means that μ → o⋅μ and σ² → o⋅[σ²+μ²(1-o)], where o the occupancy
         @transform(:omeanfrequency = :meanfrequency .* :occupancy)
-        @transform(:ovarfrequency = :varfrequency .+ :meanfrequency.^2 .* (1 .- :occupancy))
+        @transform(:ovarfrequency = :varfrequency .+ :meanfrequency .^ 2 .* (1 .- :occupancy))
         @transform(:ovarfrequency = :varfrequency .* :occupancy)
         # @transform(:meanlog = :meanlog .* :occupancy)
         # @transform(:varlog = :varlog .+ :meanlog.^2 .* (1 .- :occupancy))
         # @transform(:varlog = :varlog .* :occupancy)
         #~ Perform a log-transform on the mean-frequency (needed for lognormal)
-        @subset(:occupancy .> minoccupancy, :varfrequency .> 0.0)
+        # @subset(:occupancy .> minoccupancy, :varfrequency .> 0.0)
+        @select(idcolname, :meanfrequency, :varfrequency, :rho, :occupancy)
     end
     return sdf
 end
