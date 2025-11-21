30264f2d871c9fe833636900ccc29cd961d62f38
johannes.nauta@protonmail.com <johannes.nauta@protonmail.com>
2025-11-21 18:56:09 +0100

    [wip] Working on proper implementation of Pareto-like distributions.

--- julia-code/src/distributions/pareto.jl
+++ julia-code/src/distributions/pareto.jl
@@ -1,28 +1,215 @@
-#= Simple module with formulae for pdfs and cdfs of Pareto-like distributions =#
-#/ Start module
-module ParetoLike
+#= Simple module with formulae for pdfs and cdfs of Pareto distributions
+   So far, it includes;
+   - Pareto [type I-IV]
+     > Pareto Type I --- Pareto
+     > Pareto Type II -- Pareto II, and Lomax (for μ=0)
+     > Pareto Type III - Pareto III
+     > Pareto Type IV -- Pareto IV
+   - Generalized Pareto
+   - Burr
 
-#################
-### FUNCTIONS ###
+   Work in progress
+   [*] - Tempered Pareto
+   [*] - Truncated Pareto
+=#
+#/ Start module
+module ParetoDistribution
+
+using Distributions
+using Random
+using SpecialFunctions
+using RootSolvers
+
+###############
+### STRUCTS ###
+struct ParetoI{T<:Real} <: ContinuousUnivariateDistribution
+    α::T
+    ε::T
+
+    ParetoI{T}(α::T, ε::T) where {T<:Real} = new{T}(α, ε)
+end
+
+function ParetoI(α::T, ε::T; check_args::Bool = true) where {T<:Real}
+	  Distributions.@check_args ParetoI (α, α > 0) (ε, ε > 0)
+    return ParetoI{T}(α, ε)
+end
+
+struct ParetoII{T<:Real} <: ContinuousUnivariateDistribution
+    α::T
+    ε::T
+    θ::T
+
+    ParetoII{T}(α::T, ε::T, θ::T) where {T<:Real} = new{T}(α, ε, θ)
+end
+
+struct ParetoIII{T<:Real} <: ContinuousUnivariateDistribution
+    γ::T
+    ε::T
+    θ::T
+    
+    ParetoIII{T}(γ::T, ε::T, θ::T) where {T<:Real} = new{T}(γ, ε, θ)
+end
+
+struct ParetoIV{T<:Real} <: ContinuousUnivariateDistribution
+    α::T
+    γ::T
+    ε::T
+    θ::T
+
+    ParetoIV{T}(α, γ, ε, θ) where {T<:Real} = new{T}(α, γ, ε, θ)
+end
+
+struct Burr{T<:Real} <: ContinuousUnivariateDistribution
+    c::T
+    α::T
+    ε::T
+
+    Burr{T}(c,α,ε) where {T<:Real} = new{T}(c,α,ε)
+end
+
+function Burr(c::T, α::T, ε::T; check_args::Bool = true) where {T<:Real}
+	  Distributions.@check_args Burr (c, c > zero(c)) (α, α > zero(α)) (ε, ε > zero(ε))
+    return Burr{T}(c, α, ε)
+end
+
+struct TemperedPareto{T<:Real} <: ContinuousUnivariateDistribution
+    α::T
+    β::T
+    ε::T
+
+    TemperedPareto{T}(α,β,ε) where {T<:Real} = new{T}(α,β,ε)
+end
+
+function TemperedPareto(α::T, β::T, ε::T; check_args::Bool=true) where {T<:Real}
+	  Distributions.@check_args TemperedPareto (α, α > zero(α)) (β, β > zero(β)) (ε, ε > zero(ε))
+    return TemperedPareto{T}(α, β, ε)
+end
+
+####################
+### CONSTRUCTORS ###
+#~  Pareto distributions
+ParetoI(α::Real, ε::Real; check_args::Bool=true) = ParetoI(promote(α,ε)...; check_args=check_args)
+ParetoII(α::Real, ε::Real, θ::Real; check_args::Bool=true) = ParetoII(promote(α,ε,θ)..., check_args=check_args)
+ParetoIII(γ::Real, ε::Real, θ::Real; check_args::Bool=true) = ParetoIII(promote(γ,ε,θ)..., check_args=check_args)
+ParetoIV(α::Real, γ::Real, ε::Real, θ::Real; check_args::Bool=true) = ParetoIV(promote(α,γ,ε,θ)..., check_args=check_args)
+TemperedPareto(α::Real, β::Real, ε::Real; check_args::Bool=true) = TemperedPareto(promote(α,β,ε)..., check_args=check_args)
+
+#~ Burr distribution
+Burr(c::Real, α::Real, ε::Real; check_args::Bool=true) = Burr(promote(c,α,ε)...; check_args=check_args)
+
+
+##################
+### STATISTICS ###
+
+mean(d::ParetoI) = if d.α <= 1 return Inf else return d.α*d.ε / (d.α - 1) end
+scale(d::ParetoI) = d.ε
+shape(d::ParetoI) = d.α
+params(d::ParetoI) = (d.α, d.ε)
+
+#########################
+### DENSITY FUNCTIONS ###
+function pdf(d::ParetoI, x::Real)
+	  if x <= d.ε
+        return 0.0
+    end
+    return d.α*d.ε^d.α * x^(-(d.α+1))
+end
+
+function pdf(d::Burr, x::Real)
+	  if x <= d.ε
+        return 0.0
+    end
+    return (d.c*d.α / d.ε) * (x / d.ε)^(d.c-1) * (1 + (x / d.ε)^d.c)^(-d.α-1)
+end
+
+function pdf(d::TemperedPareto, x::Real)
+    if x <= d.ε
+        return 0.0
+    end
+	  return d.ε^d.α*exp(d.β*d.ε) * x^(-d.α-1)*exp(-d.β*x) * (d.α + d.β*x)
+end
+
+##########################
+### SURVIVAL FUNCTIONS ###
+function ccdf(d::ParetoI, x::Real)
+	  if x <= d.ε
+        return 1.0
+    end
+    return (d.ε / x)^d.α
+end
+
+function ccdf(d::Burr, x::Real)
+	  if x < d.ε
+        return 1.0
+    end
+    return (1 + (x / d.ε)^d.c)^(-d.α)
+end
+
+function ccdf(d::TemperedPareto, x::Real)
+	  if x < d.ε
+        return 1.0
+    end
+    return d.ε^d.α*exp(d.β*d.ε) * x^(-d.α)*exp(-d.β*x)
+end
+
+##########################
+### SAMPLING FUNCTIONS ###
+xval(d::Burr, u::Real) = d.ε .* ((1 .- u).^(-1/d.α) .- 1) .^ (1 / d.c)
+rand(rng::AbstractRNG, d::Burr{T}) where {T<:Real} = xval(d, Random.rand(rng,float(T)))
+function rand(rng::AbstractRNG, d::Burr{T}, n::Int) where {T<:Real}
+    U = Array{Float64}(undef, n)
+    rand!(rng, d, U)
+    return U
+end
+function rand!(rng::AbstractRNG, d::Burr, U::AbstractArray{<:Real})
+    Random.rand!(rng, U)
+    map!(Base.Fix1(xval, d), U, U)
+    return U
+end
+
+function xval(d::TemperedPareto, u::Real)
+    z = log(d.ε^d.α * exp(d.β*d.ε) / (1 - u))
+    #~ Define (function, deriviative) for NewtonsMethod
+    #! note: we do y = log(x) to avoid Newton's method to have x < 0 [see below]
+    fdf(x::Real) = (d.α*x + d.β*exp(x) - z, d.α + d.β*exp(x))
+    #~ Solve with good initial guess
+    #! note: as x>0, we need the initial guess to be "close", as otherwise the method may
+    #        overshoot into x<0 territory, so the guess must depend on `u`.
+    guess = log(max(d.ε, z / d.β))
+    sol = RootSolvers.find_zero(fdf, RootSolvers.NewtonsMethod{Float64}(guess))
+    #~ As we solved for y = log(x), exponentiate
+    return exp(sol.root)
+end
+rand(rng::AbstractRNG, d::TemperedPareto{T}) where {T<:Real} = xval(d, Random.rand(rng,float(T)))
+function rand(rng::AbstractRNG, d::TemperedPareto{T}, n::Int) where {T<:Real}
+	  U = Array{Float64}(undef, n)
+    rand!(rng, d, U)
+    return U
+end
+function rand!(rng::AbstractRNG, d::TemperedPareto{T}, U::AbstractArray{T}) where {T<:Real}
+	  Random.rand!(rng, U)
+    map!(Base.Fix1(xval, d), U, U)
+    return U
+end
 
 #######################
 ### LOG LIKELIHOODS ###
 """
-    loggeneralizedPareto(arg, μ, σ, ξ)
+    loggeneralizedPareto(d, x)
 
 Log density function of the generalized Pareto distribution, with an expansion with ξ near zero.
 """
-function loggeneralizedPareto(x::T, μ::T, σ, ξ) where T<:Real
-    z = (x - μ) / σ
-    expn = if abs(ξ) < 1e-5
+function logpdf(d::GeneralizedPareto, x::T) where T<:Real
+    z = (x - d.μ) / d.σ
+    expn = if abs(d.ξ) < 1e-5
         #~ Expansion for ξ near zero
-        -z * (ξ + 1) + (z^2) * ξ * (ξ + 1) / 2 -
-        (z^3) * (ξ^2) * (ξ + 1) / 3 +
-        (z^4) * (ξ^3) * (ξ + 1) / 4
+        -z * (d.ξ + 1) + (z^2) * d.ξ * (d.ξ + 1) / 2 -
+        (z^3) * (d.ξ^2) * (d.ξ + 1) / 3 +
+        (z^4) * (d.ξ^3) * (d.ξ + 1) / 4
     else
-        (-(1 + ξ) / ξ) * log(max(0, 1 + z * ξ))
+        (-(1 + d.ξ) / d.ξ) * log(max(0, 1 + z * d.ξ))
     end
-    return expn - log(σ)
+    return expn - log(d.σ)
 end
 
 """
@@ -30,8 +217,8 @@ end
 
 Log density function of the tempered Pareto distribution
 """
-function logtemperedPareto(x::T, α, β, xmin) where {T<:Real}
-    return α*log(xmin) + β*xmin - β*x - (1+α)*log(x) + log(α + β*x)
+function logpdf(d::TemperedPareto, x::T) where {T<:Real}
+    return d.α*log(d.ε) + d.β*d.ε - d.β*x - (1+d.α)*log(x) + log(d.α + d.β*x)
 end
 
 ############
@@ -48,20 +235,6 @@ function generalizedParetocdf(σ::Float64, ξ::Float64; xmin=0.0)
     return F
 end
 
-function temperedParetocdf(α::Float64, β::Float64; xmin=1.0)
-    F(x) = x < xmin ? 0.0 : 1 - xmin^α*exp(β*xmin)*x^(-α)*exp(-β*x)
-end
-
-function Paretocdf(α::Float64; xmin=0.0)
-    F(x) = x < xmin ? 0.0 : 1 - (xmin / x)^α
-    return F
-end
-
-function Burrcdf(c::Float64, α::Float64, λ::Float64)
-    F(x) = 1 - (1 + (x / λ)^c)^(-α)
-    return F
-end
-
 ############
 ### PDFS ###
 #~ Generalized Pareto distribution
@@ -73,33 +246,6 @@ function generalizedParetopdf(x::Float64, σ::Float64, ξ::Float64; xmin=1.0)
     (x < xmin) && (return 0.0)
     z = (x - xmin) / σ
     return (1 + ξ*z) ^ (-1 - 1 / ξ) / σ
-end
-
-#~ Pareto distribution
-function Paretopdf(x::Array{T}, α::Float64; xmin=1.0) where T<:Real
-    return Paretopdf.(x, Ref(α); xmin=xmin)
-end
-
-function Paretopdf(x::Float64, α::Float64; xmin=1.0)
-    (x < xmin) && (return 0.0)
-    return α*xmin^α * x^(-α-1)
-end
-
-#~ Burr distribution
-function Burrpdf(x::Array{T}, c::Float64, α::Float64, λ::Float64) where T<:Real
-	  return Burrpdf.(x, Ref(c), Ref(α), Ref(λ))
-end
-
-function Burrpdf(x::T, c::Float64, α::Float64, λ::Float64) where T<:Real
-    (x <= 0) && (return 0.0)
-	  return (c*α/λ) * (x/λ)^(c-1) * (1 + (x/λ)^c)^(-α-1)
-end
-
-#~ Lomax distribution
-Lomaxcdf(α::Float64, λ::Float64) = Burrcdf(1.0, α, λ)
-
-function Lomaxpdf(x::T, α::Float64, λ::Float64) where T<:Real
-    return Burrpdf.(x, 1.0, Ref(α), Ref(λ))
 end
 
 end # module ParetoLike
