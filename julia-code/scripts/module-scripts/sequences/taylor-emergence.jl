64b83c8c9ca77255b9df25fc84e5beadfe9d1bb7
johannes.nauta@protonmail.com <johannes.nauta@protonmail.com>
2026-01-15 16:21:15 +0100

    Updates synthetic Taylor's law towards plot

--- julia-code/scripts/module-scripts/sequences/taylor-emergence.jl
+++ julia-code/scripts/module-scripts/sequences/taylor-emergence.jl
@@ -7,20 +7,27 @@ using Distributions
 using StatsBase
 using Random
 using SparseArrays
+using JLD2
 
 import Meris.MDistributions as MDist
+import Meris.DATADIR as DATADIR
+TLDIR = DATADIR * "taylor/synthetic/"
 
 #################
 ### FUNCTIONS ###
+"""
+    observe
 
-########################
-### HELPER FUNCTIONS ###
+Observe system by multinomial sampling out of S components, each with prob. `p[i] = θ[i]/Θ`, where
+`Θ = sum(θ)`, and `θ ~ TemperedPareto(γ,1/φ)` with `φ` the characteristic scale of the system.
+, when `N≫φ` 
+"""
 function observe(
     N::Int, K::Int;
     γ::Float64=0.5,
     φ::Float64=1e5,
     ε::Float64=1e0,
-    S::Int=2^14,
+    S::Int=10^5,
     rng=Random.Xoshiro(42*N)
 )
     #~ Generate random propensities
@@ -35,31 +42,33 @@ function observe(
     for k in 1:K
         ξ = rand(rng, Pξ, S)
         p = w .* ξ / sum(w .* ξ)
-        n = StatsBase.sample(rng, 1:S, Weights(p), N; replace=true)
-        cmap = StatsBase.countmap(n)
-        for (label, counts) in cmap
-            x[k, label] = counts
-        end
+        # n = StatsBase.sample(rng, 1:S, Weights(p), N; replace=true)
+        x[k,:] .+= rand.(rng, Poisson.(N.*p))
+        
+        # cmap = StatsBase.countmap(n)
+        # for (label, counts) in cmap
+        #     x[k, label] = counts > 1 ? counts : 0    #~ Measurement error, exclude hapax legomena
+        # end
     end
     μx, σx = taylor(x)
-    return (; x=x, μx=μx, σx=σx)
+    return (; x=x, μx=μx, σx=σx, params=(; N=N, K=K, S=S, φ=φ, γ=γ, ε=ε))
 end
 
 ########################
 ### HELPER FUNCTIONS ###
-function taylor(x::Matrix{Int}; minoccupancy::Float64=0.1)
+function taylor(x::Matrix{Int}; minoccupancy::Float64=0., scale=false)
     K, S = size(x)
     #/ Compute mean and variance
     #~ Filter by occupancy
     occ = map(col -> count(!iszero, col), eachcol(x)) ./ K
     nzidx = findall(o -> o >= minoccupancy, occ)
-    xnz = @view x[:,nzidx]
+    xnz = scale ? (@view x[:,nzidx]) : (@view x[:,:])
     μx = map(col -> mean(col), eachcol(xnz))
     σx = map(col -> var(col),  eachcol(xnz))
     return μx, σx
 end
 
-function taylor(x::SparseMatrixCSC; minoccupancy::Float64=0.1)
+function taylor(x::SparseMatrixCSC; minoccupancy::Float64=0.01)
     K, S = size(x)
     #/ Compute mean and variance
     #~ Filter by occupancy
@@ -85,6 +94,13 @@ function ftaylor(x::SparseMatrixCSC, N::Int; samplingdepth::Float64 = 10.0)
     σx = map(col -> var(col),  eachcol(x))
     fidxs = findall(μ -> μ > samplingdepth, μx)
     return μx[fidxs], σx[fidxs]
+end
+
+#############################
+### DATA HELPER FUNCTIONS ###
+function save_taylor(result, filename; DIR=TLDIR)
+    mkpath(DIR)
+    jldsave(DIR*filename, n=result.x, mean=result.μx, var=result.σx, params=result.params)
 end
 
 end # module TaylorSampler
