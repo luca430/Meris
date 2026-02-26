be607f179bfd2d2abf95d43898bdf1c3fdc3efa9
Johannes Nauta <johannes.nauta@ugent.be>
2026-02-26 09:36:56 +0100

    Fixes CLI calls [again]

--- julia-code/scripts/cli-scripts/goodness-of-fit/fitcandidates.sh
+++ julia-code/scripts/cli-scripts/goodness-of-fit/fitcandidates.sh
@@ -7,10 +7,10 @@ NEPSILON=100
 NTOP=50
 # tip: check `julia --project [name].jl --help` for arg options
 # julia --threads 8 --project arxiv.jl -f -n $NEPSILON --top $NTOP
-# julia --threads 8 --project finance.jl -f -n $NEPSILON --top $NTOP
+julia --threads 8 --project finance.jl -f -n $NEPSILON --top $NTOP
 # julia --threads 8 --project gowalla.jl -f -n $NEPSILON --top $NTOP
-julia --threads 8 --project gutenberg.jl -f -n $NEPSILON --top $NTOP
-julia --threads 8 --project lego.jl -f -n $NEPSILON --top $NTOP
-julia --threads 8 --project rfc.jl -f -n $NEPSILON -top $NTOP
+# julia --threads 8 --project gutenberg.jl -f -n $NEPSILON --top $NTOP
+# julia --threads 8 --project lego.jl -f -n $NEPSILON --top $NTOP
+# julia --threads 8 --project rfc.jl -f -n $NEPSILON --top $NTOP
 
 
